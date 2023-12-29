867b71d2cc20369cdda056885d4ebb82e32078fc
Rajarshi Dutta <gogolmf74@gmail.com>
2023-12-29 13:47:42 +0530

    first commit

---
+++ src/components/PlayButton.jsx
@@ -0,0 +1,15 @@
+
+const PlayButton = () => {
+    return (
+        <div class="box">
+        
+            <div class="play-button">
+                
+                <i id="playbutton--icon" class='bx bx-play'></i>
+
+            </div>
+        </div>
+    )
+}
+
+export default PlayButton
